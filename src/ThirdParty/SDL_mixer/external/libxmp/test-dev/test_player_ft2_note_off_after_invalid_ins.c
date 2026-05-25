f8a530923b0fffaa2845f921d683e81b40068ab2
andrescolombo <andres.colombo@gmail.com>
2026-05-25 18:17:50 +0200

    chore: remove incomplete friend SDL/SDL_mixer copies (use submodules instead)

--- src/ThirdParty/SDL_mixer/external/libxmp/test-dev/test_player_ft2_note_off_after_invalid_ins.c
+++
@@ -1,9 +0,0 @@
-#include "test.h"
-
-TEST(test_player_ft2_note_off_after_invalid_ins)
-{
-	compare_mixer_data(
-		"data/ft2_note_off_after_invalid_ins.xm",
-		"data/ft2_note_off_after_invalid_ins.data");
-}
-END_TEST
